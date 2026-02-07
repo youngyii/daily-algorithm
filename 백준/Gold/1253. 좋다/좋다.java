99d2411d681756a23c452b24986b8c0afa1777c4
Shinyoung <104247324+youngyii@users.noreply.github.com>
2026-02-07 21:38:23 +0900

    [Gold IV] Title: 좋다, Time: 292 ms, Memory: 22204 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -16,22 +16,20 @@ public class Main {
 
         int cnt = 0;
         int sum;
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = 0; i < n; i++) {
             int start = 0;
             int end = n - 1;
             while (start < end) {
-                if (end == i) {
-                    end--;
-                    continue;
-                } else if (start == i) {
-                    start++;
-                    continue;
-                }
-
                 sum = arr[start] + arr[end];
                 if (sum == arr[i]) {
-                    cnt++;
-                    break;
+                    if (end != i && start != i) {
+                        cnt++;
+                        break;
+                    } else if (start == i) {
+                        start++;
+                    } else if (end == i) {
+                        end--;
+                    }
                 } else if (sum < arr[i]) {
                     start++;
                 } else {
