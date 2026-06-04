1c74553611136f291bbad1956025bbbb11dc01f4
Shinyoung <104247324+youngyii@users.noreply.github.com>
2026-06-05 00:32:13 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    
+    answer += my_string.substr(0, s);
+    answer += overwrite_string;
+    answer += my_string.substr(s + overwrite_string.size());
+    
+    return answer;
+}
